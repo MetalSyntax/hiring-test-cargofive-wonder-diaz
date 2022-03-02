fa400ca31ece733aec70fb87825fac7a0a383c2e
Wonder Diaz <wonderdxk@gmail.com>
2022-03-01 21:45:11 -0400

    Init hiring test
    Clean default code
    Add dependencies like Bootstrap
    Add logo and NavBar
    Add Table with Data using Axios

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar class="px-2" type="light" variant="light">
+    <b-navbar-brand href="#">
+      <img src="../assets/logo.png" alt="Logo" width="100">
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <b-table striped hover :items="items"></b-table>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+let url = 'http://apitest.cargofive.com/api/ports'
+export default {
+  name: 'Table',
+  data() {
+    axios.get(url)
+    .then(response => {
+        this.items = response.data.data
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    return {
+      items: []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
